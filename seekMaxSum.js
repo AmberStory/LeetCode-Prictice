5c2a9156752fc62376ed226064dd090a534a6448
heziruo <heziruo@meituan.com>
2020-01-19 20:42:18 +0800

    update

--- seekMaxSum.js
+++ seekMaxSum.js
@@ -33,15 +33,15 @@ function seekMaxSum(arr) {
 seekMaxSum([-2,1,-3,4,-1,2,1,-5,4]);
 
 // 解法二：采用动态规划
+// 遍历以某个节点为结束点的所有子序列，比如数组[a, b, c, d]，先遍历以a为结束点的所有子序列，再遍历以b为结束点的所有子序列，再遍历以c为结束点的所有子序列，以此类推。存储每个结束点时的最大子序列和，公式可以推导为sum(i)=max(sum(i-1)+arr[i], sum[i-1])，其中sum[i]是以arr[i]为结尾的最大序列连续和。
 function seekMaxSumDynamic(arr) {
   if (!Array.isArray(arr)) return;
   let sum = arr[0];
   let max = arr[0];
   const len = arr.length;
   for(let i=1; i<len; i++) {
-    let tmp = sum + arr[i];
     // 第一个子序列：如果前i-1项的和加起来比第i项的值小，那么第i项就下一个子序列的起点；反之，如果如果前i-1项的和加起来比第i项的值大，那么该子序列可以继续向后添加元素，第i项不适合作为下一个子序列的起点。
-    sum = tmp > arr[i] ? tmp : arr[i];  
+    sum = Math.max(sum + arr[i], arr[i]); 
     max = Math.max(sum, max);
   }
   return max;
